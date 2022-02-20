7f079bd8bb9f9bc1e02291a1c07832b19f5f7ce7
Alex Bourne <afb989@outlook.com>
2022-02-19 19:46:48 -0500

    chapter 13 exercise complete

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,8 +1,6 @@
 package org.launchcode.codingevents.models;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 /**
@@ -13,23 +11,44 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
-    @NotBlank
+    @NotBlank(message = "Name is required.")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
 
-    @Size(max = 500, message = "Description too long!")
+    @Size(max = 500, message = "Description too long! Must be less than 500 characters.")
     private String description;
 
+    @NotBlank(message = "Email is required.")
     @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
-    public Event(String name, String description, String contactEmail) {
+    @NotBlank(message = "Please provide a location for this event.")
+    @Size(max = 100, message = "Must be less than 100 characters.")
+    private String location;
+
+    @AssertTrue
+    private boolean registration = true;
+
+    @NotBlank
+    private String attendees;
+
+    @AssertTrue
+    private boolean maskRequired = true;
+
+    public Event(String name, String description, String contactEmail, String location,
+                 boolean registration, String attendees, boolean maskRequired) {
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
+        this.location = location;
+        this.registration = registration;
+        this.attendees = attendees;
+        this.maskRequired = maskRequired;
         this.id = nextId;
         nextId++;
     }
+
+    public Event(){}
 
     public String getName() {
         return name;
@@ -53,6 +72,38 @@ public class Event {
 
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean isRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(boolean registration) {
+        this.registration = registration;
+    }
+
+    public String getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(String attendees) {
+        this.attendees = attendees;
+    }
+
+    public boolean isMaskRequired() {
+        return maskRequired;
+    }
+
+    public void setMaskRequired(boolean maskRequired) {
+        this.maskRequired = maskRequired;
     }
 
     public int getId() {
